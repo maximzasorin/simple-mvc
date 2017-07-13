96d2d647b69539c0cf132e564089c35582c26a75
maximzasorin <maximzasorin@gmail.com>
2017-07-13 09:23:30 +0300

    improve command resolver

--- src/Commands/CommandResolver.php
+++ src/Commands/CommandResolver.php
@@ -6,15 +6,22 @@ use Http\Request;
 
 class CommandResolver
 {
-	protected $defaultCommand = 'Commands\\NotFound';
+	protected $notFoundCommand = 'NotFound';
+	
+	protected $indexCommand = 'Index';
 
 	public function getCommand(Request $request)
 	{
 		try {
-			$commandName = $request->getProperty('command');
+			$commandName = substr(strtolower($request->getPath()), 1);
+
+			if (!$commandName) {
+				$commandName = $this->indexCommand;
+			}
+
 			$commandReflection = new \ReflectionClass('Commands\\' . $commandName);
 		} catch (\ReflectionException $exception) {
-			$commandReflection = new \ReflectionClass($this->defaultCommand);
+			$commandReflection = new \ReflectionClass('Commands\\' . $this->notFoundCommand);
 		}
 
 		return $commandReflection->newInstance();
--- src/Http/Request.php
+++ src/Http/Request.php
@@ -5,10 +5,21 @@ namespace Http;
 class Request
 {
 	protected $request = [];
+	protected $path = '/';
 
 	public function __construct()
 	{
 		$this->request = $_REQUEST;
+
+		$url = parse_url($_SERVER['REQUEST_URI']);
+		$this->path = isset($url['path'])
+			? $url['path']
+			: '/';
+	}
+
+	public function getPath()
+	{
+		return $this->path;
 	}
 
 	public function getProperty($property)
